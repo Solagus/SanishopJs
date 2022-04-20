01b821a5d105f55a5e1b39a11669bf435c909df2
Sol Ferraro <solagus0501@gmail.com>
2022-04-20 14:23:55 -0300

    division de paginas

--- prueba.js
+++ prueba.js
@@ -25,77 +25,8 @@ function actualizarDom(div, cantidades) {
     cantidad.innerHTML = cantidades
 }
 
-let divCarrito = document.getElementById("carrito")
-
-let li
-
-function crearDropdown(){
-    li = document.createElement("li")
-    li.innerHTML = `
-          <div class="filaDelCarrito d-flex justify-content-evenly">
-            <p class="parrafo" >${carrito.nombre}<p>
-            <p class="parrafo" >${carrito.cantidad} und.<p>
-            <p class="parrafo" >$${carrito.precio}<p>
-          <div>
-    `
-    divCarrito.append(li)
-}
-function actualizarCarrito(div, carrito){
-    let actualizar = div.querySelector(".filaDelCarrito")
-    actualizar.innerHTML = carrito.nombre
-}
-
-
-function crearCard(producto) {
-    let div = document.createElement('div')
-    div.innerHTML = `
-        <div class="card mb-3 p-3 border" style="width: 18rem">
-            <img src="${producto.imagen}" class="card-img-top" alt="..." style="height:20em">
-            <div class="card-body">
-               <div class="titulo">
-                   <h5 class="card-title">${producto.nombre}</h5>
-               </div>
-               <div class="precio">
-                   <p class="card-text">$${producto.precio}</p>
-               </div>
-               <div class="botones-cantidad d-flex p-2">
-                   <button id="restar${producto.id}" class="btn btn-sm btn-card px-1" style="width: 2em" >-</button>
-                   <p class="card-text px-1 cantidad">${producto.cantidad}</p>
-                   <button id="sumar${producto.id}" class="btn btn-sm btn-card">+</button>
-               </div>
-               <div>
-                   <button id="comprar${producto.id}" class="btn btn-card btn-comprar">Comprar
-               <div>
-        </div>
-    `
-    document.getElementById("containerCard").append(div)
-
-    //botones
-    let suma = document.getElementById(`sumar${producto.id}`)
-    suma.onclick = () => {
-        if (producto.cantidad < producto.maximo) {
-            producto.cantidad++;
-            actualizarDom(div, producto.cantidad)
-        }
-    }
-    let resta = document.getElementById(`restar${producto.id}`)
-    resta.onclick = () => {
-        if (producto.cantidad > 0) {
-            producto.cantidad--;
-            actualizarDom(div, producto.cantidad)
-        }
-    }
-    let comprar = document.getElementById(`comprar${producto.id}`)
-    comprar.onclick = () => {
-        carrito.push(producto)
-        console.log(carrito)
-        crearDropdown(divCarrito, li)
-    }
-
-}
-
-
 for (i = 0; i < productos.length; i++) {
     crearCard(productos[i])
 }
+
 
