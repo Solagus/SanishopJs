d973ab30a7ee6aa07eaf94849691b35470e6ce24
Sol Ferraro <solagus0501@gmail.com>
2022-04-19 23:33:05 -0300

    imagenes

--- prueba.js
+++ prueba.js
@@ -10,12 +10,12 @@ class Producto {
 }
 
 let productos = [
-    new Producto(0, "./imgenes/griferia-fv/california.jpg", "Griferia California", 1, 5, 10000),
-    new Producto(1, "./imgenes/Vanitorys/vanitory1.jpeg", "Vanitory 50X40X80", 1, 3, 13000),
-    new Producto(2, "./imgenes/griferia-fv/Epuyen.jpg", "Griferia Epuyen", 1, 2, 15000),
-    new Producto(3, "./imgenes/Accesorios/jabonera-dominic.jpg", "Jabonera Dominic", 1, 6, 5000),
-    new Producto(4, "./imgenes/Asiento-de-inodoro/tapa-de-asiento-florencia.jpg", "Asiento Florencia", 1, 4, 10000),
-    new Producto(5, "./imgenes/Accesorios/portarrollos-urbano.jpg", "Portarrollos Urbano", 1, 5, 4000)
+    new Producto(0, "./imagenes/griferia-fv/california.jpg", "Griferia California", 1, 5, 10000),
+    new Producto(1, "./imagenes/Vanitorys/vanitory1.jpeg", "Vanitory 50X40X80", 1, 3, 13000),
+    new Producto(2, "./imagenes/griferia-fv/Epuyen.jpg", "Griferia Epuyen", 1, 2, 15000),
+    new Producto(3, "./imagenes/Accesorios/jabonera-dominic.jpg", "Jabonera Dominic", 1, 6, 5000),
+    new Producto(4, "./imagenes/Asiento-de-inodoro/tapa-de-asiento-florencia.jpg", "Asiento Florencia", 1, 4, 10000),
+    new Producto(5, "./imagenes/Accesorios/portarrollos-urbano.jpg", "Portarrollos Urbano", 1, 5, 4000)
 ]
 
 let carrito = []
