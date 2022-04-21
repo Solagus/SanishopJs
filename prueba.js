4c6405a773bfafb8680b9fd5408142ae4c8f2c38
Sol Ferraro <solagus0501@gmail.com>
2022-04-21 19:19:52 -0300

    modificacion de botones

--- prueba.js
+++ prueba.js
@@ -9,6 +9,7 @@ class Producto {
     }
 }
 
+
 let productos = [
     new Producto(0, "./imagenes/griferia-fv/california.jpg", "Griferia California", 1, 5, 10000),
     new Producto(1, "./imagenes/Vanitorys/vanitory1.jpeg", "Vanitory 50X40X80", 1, 3, 13000),
@@ -18,15 +19,102 @@ let productos = [
     new Producto(5, "./imagenes/Accesorios/portarrollos-urbano.jpg", "Portarrollos Urbano", 1, 5, 4000)
 ]
 
+
 let carrito = []
+
+//⁡⁣⁢⁣​‌‍‌𝕕𝕣𝕠𝕡𝕕𝕠𝕨𝕟​⁡
+
+let li
+let divCarrito = document.getElementById("carrito")
+let divDelLi =document.getElementById("lista")
+
+function crearDropdown(divi, divj){
+    for (let i of carrito){
+        li = document.createElement("li")
+        li.innerHTML = `
+          <div class="filaDelCarrito d-flex justify-content-evenly">
+            <p class="parrafo" >${i.nombre}<p>
+            <p class="parrafo" >${i.cantidad} und.<p>
+            <button class="btn" id="btn-eliminar${i.id}"><img src="./imagenes/105739.png" style="width: 1em; height: 1em; margin-top: -0.8em;" alt=""></button>
+            <p class="parrafo">$${i.precio*i.cantidad}<p>
+          <div>
+          `
+        divi.append(li)
+    }
+    console.log(carrito)
+}
+
+//⁡⁣⁢⁣​‌‍‌𝕗𝕚𝕟𝕒𝕝𝕚𝕫𝕒 𝕖𝕝 𝕕𝕣𝕠𝕡𝕕𝕠𝕨𝕟​⁡
+
+//⁡⁢⁣⁣​‌‍‌𝕔𝕒𝕣𝕕𝕤 𝕪 𝕓𝕠𝕥𝕠𝕟𝕖𝕤​⁡
 
 function actualizarDom(div, cantidades) {
     let cantidad = div.querySelector(".cantidad")
     cantidad.innerHTML = cantidades
 }
+function crearCard(producto) {
+    let div = document.createElement('div')
+    div.innerHTML = `
+        <div class="card mb-3 p-3 border" style="width: 18rem">
+            <img src="${producto.imagen}" class="card-img-top" alt="..." style="height:20em">
+            <div class="card-body">
+               <div class="titulo">
+                   <h5 class="card-title">${producto.nombre}</h5>
+               </div>
+               <div class="precio">
+                   <p class="card-text">$${producto.precio}</p>
+               </div>
+               <div class="botones-cantidad d-flex p-2">
+                   <button id="restar${producto.id}" class="btn btn-sm btn-card px-1" style="width: 2em" >-</button>
+                   <p class="card-text px-1 cantidad">${producto.cantidad}</p>
+                   <button id="sumar${producto.id}" class="btn btn-sm btn-card">+</button>
+               </div>
+               <div>
+                   <button id="comprar${producto.id}" class="btn btn-card btn-comprar">Comprar
+               <div>
+        </div>
+        
+    `
+    document.getElementById("containerCard").append(div)
+    
+    //​‌‍‌⁡⁢⁢⁣⁡⁢⁣⁢𝕓𝕠𝕥𝕠𝕟𝕖𝕤⁡⁡​
+    
+    let suma = document.getElementById(`sumar${producto.id}`)
+    suma.onclick = () => {
+        if (producto.cantidad < producto.maximo) {
+            producto.cantidad++;
+            actualizarDom(div, producto.cantidad)
+        }
+    }
+    let resta = document.getElementById(`restar${producto.id}`)
+    resta.onclick = () => {
+        if (producto.cantidad > 0) {
+            producto.cantidad--;
+            actualizarDom(div, producto.cantidad)
+        }
+    }
+    let comprar = document.getElementById(`comprar${producto.id}`)
+    comprar.onclick = () => {
+        if (carrito.find((el) => el.nombre == producto.nombre)){
+            producto.cantidad++;
+        }
+        else{
+            carrito.push(producto)
+        }
+        console.log(carrito)
+        divDelLi.innerHTML = ""
+        crearDropdown(divDelLi, divCarrito)
+    
+    }
+
+}
+
+//​‌‍‌⁡⁢⁣⁣𝕗𝕚𝕟𝕒𝕝𝕚𝕫𝕒𝕟 𝕔𝕒𝕣𝕕𝕤 𝕪 𝕓𝕠𝕥𝕠𝕟𝕖𝕤⁡​
+
 
 for (i = 0; i < productos.length; i++) {
     crearCard(productos[i])
 }
 
-
+let carritoAJson = JSON.stringify(carrito)
+localStorage("carrito", carritoAJson)
